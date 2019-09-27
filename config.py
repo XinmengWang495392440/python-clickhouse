990f7aee48e03900fe66b645913f67ac31321d07
wangxm <wangxm@craiditx.com>
2019-09-27 10:03:49 +0000

    first version

---
+++ config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+
+import logging
+import sys 
+
+# print(sys.executable)
+
+display ('input current date: ')
+time = input()
+
+log_settings = {
+    'filename': 'log/log_{}'.format(time),
+    'format': '%(asctime)s - %(pathname)s:%(lineno)d - ' \
+        '%(levelname)s: %(message)s',
+#    'mode':'a',
+    'level': logging.INFO,
+}
+
+
+logging.basicConfig(**log_settings)
+
+
+
+        
+        
+        
+        
+        
---
+++ s1_prepare_data.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+import config
+import pandas as pd
+import getpass
+from clickhouse_driver import Client
+import logging
+
+
+class Prepare_Data():
+    
+    def __init__(self, host = 'localhost', port = '9000', user = 'default'):
+        self.host = host
+        self.port = port
+        self.user = user
+        
+        
+    def login_and_choose_database(self, database):
+        self.client = Client(host = self.host, port = self.port, database = database, user = self.user)
+        logging.info('login database: %s', database)
+
+
+    def raw_query(self, query): 
+        logging.info('run query: %s', query)
+        ans = self.client.execute(query)
+        return ans
+
+
+    def create_table(self, table_name, columns, engine, ordered_column):
+        logging.info('create table: %s', table_name)
+        sql = [
+        "CREATE TABLE IF NOT EXISTS " + table_name + '\n'
+        " (" + columns + ")" + '\n'
+        " ENGINE = " + engine +'\n'
+        " ORDER BY " + ordered_column + '\n' 
+        ]
+        query = ''.join(sql)
+        print ('query overview:')
+        print (query)
+        self.client.execute(query)
+        print ('show tables:')
+        print (self.client.execute('show tables;'))
+        
+    
+    def get_random_sample(self, current_table, new_table, num_cases, ordered_column, selected_columns, engine = 'MergeTree'):
+        logging.info('create sample table: %s', new_table)
+        sql = [
+        "CREATE TABLE IF NOT EXISTS " + new_table + '\n'
+        " ENGINE = " + engine +'\n'
+        " ORDER BY " + ordered_column + '\n' 
+        " AS SELECT " + selected_columns + '\n'
+        " FROM " + current_table + '\n'
+        " ORDER BY rand() \n"
+        " LIMIT " + num_cases + '\n'
+        ]
+        query = ''.join(sql)
+        print ('query overview:')
+        print (query)
+        self.client.execute(query)
+        print ('show tables:')
+        print (self.client.execute('show tables;'))
+        
+        
+    def get_dataframe_head(self, table_name, columns = None):
+        sql = [
+        "SELECT * FROM " + table_name + '\n'
+        " LIMIT 5"]
+        query = ''.join(sql)
+        res = self.client.execute(query)
+        res_dataframe = pd.DataFrame(res, columns = columns)
+        display (res_dataframe)
+            
+        
+        
+        
+        
+        
+        
+        
